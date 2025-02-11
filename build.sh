90ae5a88dd5859dc2f7b093614c335d1548dcbb0
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2025-02-11 17:34:44 +0100

    fix build script

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 git submodule update --init --recursive
 # TODO for ros2
-# sudo apt update
-# sudo apt install libenchant-2-dev -y python3-empy zip
-# python3 -m pip install --upgrade pip
-# apt install python3.8-venv -y
-# python3 -m venv docs-env
-# ls
-# ls docs-env
-# ls docs-env/bin
-# . docs-env/bin/activate
-# pip install -r requirements.txt
+sudo apt update
+sudo apt install libenchant-2-dev -y python3-empy zip
+python3 -m pip install --upgrade pip
+apt install python3.10-venv -y
+python3 -m venv docs-env
+ls
+ls docs-env
+ls docs-env/bin
+. docs-env/bin/activate
+pip install -r requirements.txt
 # cd _modules/mirte-python
 # pip install .
 # pip install empy==3.3.4
